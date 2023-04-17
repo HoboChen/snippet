80a6c9aa1e14dcf6760c0b068a710de720207133
Hobo Chen <hobochen96@live.com>
2023-04-17 14:44:30 -0700

    change tmux theme

--- config/hobozshrc/recover.sh
+++ config/hobozshrc/recover.sh
@@ -3,7 +3,6 @@
 cp hobozshrc ~/.zshrc
 cp hobotmuxconf ~/.tmux.conf
 cp hobotmuxconflocal ~/.tmux.conf.local 
-cp af-magic.zsh-theme /home/hobochen/.oh-my-zsh/themes/af-magic.zsh-theme
 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
