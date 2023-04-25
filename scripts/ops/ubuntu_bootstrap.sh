cccb612a7a11968d43a6bb6d9b180c2d65ce60d5
Hobo Chen <hobochen96@live.com>
2023-04-25 13:28:49 -0700

    make some install optional and install tldr globally; and rename file

--- scripts/ops/ubuntu_bootstrap.sh
+++ scripts/ops/ubuntu_bootstrap.sh
@@ -4,8 +4,8 @@ sudo apt upgrade -y
 sudo apt install sysstat htop iftop wget tmux curl zsh ripgrep \
   lm-sensors pandoc \
   vim git build-essential python3 python3-pip
-sudo apt install gnome-tweak-tool net-tools
-sudo apt install openjdk-17-jdk clang ghc
+# sudo apt install gnome-tweak-tool net-tools
+# sudo apt install openjdk-17-jdk clang ghc
 # sudo apt install texlive-full
 
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
@@ -14,5 +14,5 @@ python3 -m pip install --user --upgrade pip
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 nvm install --lts
-npm install tldr
+npm install tldr -g
 
