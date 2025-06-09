075062205edf9db01c27494a4feb430d727b193e
Hobo Chen <hobochen96@live.com>
2025-06-08 17:01:01 -0700

    add test for ubuntu rime

--- scripts/ops/ubuntu_rime.sh
+++ scripts/ops/ubuntu_rime.sh
@@ -6,3 +6,7 @@ XMODIFIERS      DEFAULT=@im=fcitx" >> ~/.pam_environment
 
 mkdir -p ~/.config/autostart
 cp /usr/share/applications/org.fcitx.Fcitx5.desktop ~/.config/autostart/
+
+# echo $XMODIFIERS    # should now print "@im=fcitx"
+# echo $GTK_IM_MODULE # should now print "fcitx"
+# echo $QT_IM_MODULE  # should now print "fcitx"
