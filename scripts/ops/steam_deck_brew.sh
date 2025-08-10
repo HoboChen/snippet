8c611631e0385db1334d9f6f2a07c75e43b9e6e8
Hobo Chen <hobochen96@live.com>
2025-08-10 16:26:28 -0700

    add steam deck brew installation script

---
+++ scripts/ops/steam_deck_brew.sh
@@ -0,0 +1,7 @@
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)" >> ~/.zshrc
+
+brew install glib
+
+brew install gcc
